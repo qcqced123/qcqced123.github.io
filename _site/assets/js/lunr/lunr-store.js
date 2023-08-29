09a34d6f63e6040665492116cc93e9834602e4e1
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-08-29 15:01:52 +0900

    Add PS Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -154,4 +154,10 @@ var store = [{
         "tags": ["Python","Codeing Test","Algorithm","Baekjoon","String Handle"],
         "url": "/algorithm/baekjoon-1969",
         "teaser": "/assets/images/huggingface_emoji.png"
+      },{
+        "title": "👩‍💻🔠 [baekjoon] 17609번: 회문",
+        "excerpt":"🖍️ solution import sys \"\"\" [시간] 1) 14:20 ~ 14:45 [요약] 1) 유사회문: 한 문자를 삭제하여 회문으로 만들 수 있는 문자열 =&gt; 유사회문인지 아닌지 판단하는 프로그램 작성 2) 주어진 문자열의 길이는 10만, 문자열 개수는 최대 30개 =&gt; 제한 시간이 1초라서 O(n)의 알고리즘을 설계 필요, Counter 사용 불가 [전략] 1) 슬라이싱 이용해서...","categories": ["Algorithm"],
+        "tags": ["Python","Codeing Test","Algorithm","Baekjoon","String Handle"],
+        "url": "/algorithm/baekjoon-17609",
+        "teaser": "/assets/images/huggingface_emoji.png"
       }]
