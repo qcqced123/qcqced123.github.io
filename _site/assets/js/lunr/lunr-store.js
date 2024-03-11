8133c0a6cee817948b980918d594a0a94d965346
JungHwan Lee <qcqced123@gmail.com>
2024-03-11 20:00:01 +0900

    add

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -293,6 +293,12 @@ var store = [{
         "url": "/nlp/electra",
         "teaser": "/assets/images/huggingface_emoji.png"
       },{
+        "title": "🌆 [Roformer] RoFormer: Enhanced Transformer with Rotary Position Embedding",
+        "excerpt":"🔭 Overview DistilBERT 는 허깅 페이스 연구진이 2019년 발표한 BERT의 변형으로서, On-Device Ai 개발을 목표로 경량화에 초점을 맞춘 모델이다. GPT, BERT의 등장 이후, NLP 분야에서 비약적인 성능 향상이 이뤄졌음에도 불구하고, 터무니 없는 모델 사이즈와 컴퓨팅 리소스 요구로 인해 실생활 적용 같은 활용성은 여전히 해결해야할 문제로 남아 있었다. Google에서 발표한 초기 BERT-base-uncased...","categories": ["NLP"],
+        "tags": ["Natural Language Process","Roformer","Linear-Attention","Self-Attention","Pytorch","Transformation Matrix","Complex Space"],
+        "url": "/nlp/roformer",
+        "teaser": "/assets/images/huggingface_emoji.png"
+      },{
         "title": "🌆 [SpanBERT] SpanBERT: Improving Pre-training by Representing and Predicting Spans",
         "excerpt":"🔭 Overview SpanBERT는 2020년 페이스북에서 발표한 BERT 계열 모델로, 새로운 방법론인 SBO(Span Boundary Objective)를 고안해 사전학습을 하여 기존 대비 높은 성능을 기록했다. 기존 MLM, CLM은 단일 토큰을 예측하는 방식을 사용하기 때문에 Word-Level Task에 아주 적합하지만 상대적으로 QA, Sentence-Similarity 같은 문장 단위 테스크에 그대로 활용하기에는 부족한 점이 있었다. 이러한 문제를 해결하기 위해...","categories": ["NLP"],
         "tags": ["Natural Language Process","SpanBERT","BERT","Self-Attention","Pytorch"],
